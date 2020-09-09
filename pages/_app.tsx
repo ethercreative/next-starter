d170c0826ca1d5500b7fa172201cbf4b7e93c0fc
James Edmonston <james@ethercreative.co.uk>
2020-09-09 12:12:17 +0100

    Fix _app.tsx React import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import type { AppProps } from 'next/app';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
