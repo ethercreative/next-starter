c3e368fe959c006b0a69ab3d0aa7a20450955797
James Edmonston <james@ethercreative.co.uk>
2020-09-11 23:57:52 +0100

    Add HTML lang

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
