df3c2b3d9a8e276622eb33916420e6a9c719eefa
James Edmonston <james@ethercreative.co.uk>
2020-10-07 17:37:26 +0100

    Add ether-ui to purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   purge: {
-    content: ['./out/**/*.html', './components/**/*.tsx', './pages/**/*.tsx'],
+    content: [
+      './out/**/*.html',
+      './components/**/*.tsx',
+      './pages/**/*.tsx',
+      './node_modules/ether-ui/**/*.tsx',
+    ],
     preserveHtmlElements: false,
     options: {
       keyframes: true,
