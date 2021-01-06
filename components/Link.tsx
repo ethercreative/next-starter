6918fddef1b6431e78ecba79e24d3ceff7b99bcc
James Edmonston <james@ethercreative.co.uk>
2021-01-06 17:31:02 +0000

    Update Link.tsx

--- components/Link.tsx
+++ components/Link.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 
 interface Props {
   href: string;
-  as?: string;
   scroll?: boolean;
   className?: string;
   style?: React.CSSProperties;
@@ -11,13 +10,12 @@ interface Props {
 
 const Anchor: React.FC<Props> = ({
   href = '',
-  as = '',
   scroll = true,
   className = '',
   style = {},
   children = null,
 }) => (
-  <Link href={href} as={as} scroll={scroll}>
+  <Link href={href} scroll={scroll}>
     <a className={className} style={style}>
       {children}
     </a>
