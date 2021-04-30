41805d0accfcd448e66b91dcca3ad91fd8b668ed
James Edmonston <james@ethercreative.co.uk>
2021-04-30 12:55:46 +0100

    Fix client previewData type with Next 20.2

--- client.ts
+++ client.ts
@@ -2,13 +2,18 @@ import { GetStaticPropsContext } from 'next';
 import { GraphQLClient } from 'graphql-request';
 import { ParsedUrlQuery } from 'querystring';
 
+interface PreviewData {
+  previewToken?: string;
+}
+
 export const client = (
   context?: GetStaticPropsContext<ParsedUrlQuery>,
 ): GraphQLClient => {
   let endpoint = process.env.GRAPH_URL ?? '';
+  const previewData = (context?.previewData as PreviewData) ?? null;
 
-  if (context?.previewData?.previewToken) {
-    endpoint += `?token=${context.previewData?.previewToken}`;
+  if (previewData?.previewToken) {
+    endpoint += `?token=${previewData?.previewToken}`;
   }
 
   return new GraphQLClient(endpoint, {
