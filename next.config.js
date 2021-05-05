36cda826f4776bed4c2ce96dd64ca0be052eaf53
James Edmonston <james@ethercreative.co.uk>
2021-05-05 12:59:15 +0100

    Improve default security headers

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ module.exports = withPreact({
             key: 'Permission-Policy',
             value: 'interest-cohort=()',
           },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1',
+          },
         ],
       },
     ];
