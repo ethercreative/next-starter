d0b0b5d0739d02eed3151f3447c1983d4339ca47
James Edmonston <james@ethercreative.co.uk>
2021-06-17 22:41:54 +0100

    Remove useBoolean

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,3 @@
-export { useBoolean } from './useBoolean';
 export { useFormReducer } from './useFormReducer';
 export { useMounted } from './useMounted';
 export { useNetwork } from './useNetwork';
