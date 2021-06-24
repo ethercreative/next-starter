4c4d986c6c6525e1fa06966a9c39c157219af4fb
James Edmonston <james@ethercreative.co.uk>
2021-06-24 11:21:27 +0100

    Remove Prose

--- components/index.ts
+++ components/index.ts
@@ -4,4 +4,3 @@ export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Link } from './Link';
 export { default as Page } from './Page';
-export { default as Prose } from './Prose';
