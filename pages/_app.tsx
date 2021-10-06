1f9dea4f55fc2d36d7bd900e19b9bc151c622e3e
James Edmonston <james@ethercreative.co.uk>
2021-10-06 17:00:47 +0100

    Add favicon link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Footer, Header } from '@/components';
 import '../public/style.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <div className='grid gap-y-16 md:gap-y-24'>
-    <Header />
-    <Component {...pageProps} />
-    <Footer />
-  </div>
+  <>
+    <Head>
+      <link rel='icon' href='/assets/favicon.svg' />
+    </Head>
+
+    <div className='grid gap-y-16 md:gap-y-24'>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  </>
 );
 
 export default App;
