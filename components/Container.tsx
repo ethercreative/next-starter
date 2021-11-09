d8d96db4066383c25204a41dcead84f06b72d64c
James Edmonston <james@ethercreative.co.uk>
2021-11-09 12:24:20 +0000

    Extend container width

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
 export const Container: React.FC = ({ children }) => (
-  <div className='w-full xl:container px-6 md:px-10 mx-auto'>{children}</div>
+  <div className='w-full 2xl:container px-6 md:px-10 mx-auto'>{children}</div>
 );
