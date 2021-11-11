c29275835a150cb3de4cf230085407acefc7ff5d
James Edmonston <james@ethercreative.co.uk>
2021-11-11 10:14:47 +0000

    Embed GA if NEXT_PUBLIC_GA_TAG present

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import { Footer, Header } from '@/components';
 import '../public/style.css';
 
@@ -15,6 +16,31 @@ const App = ({ Component, pageProps }: AppProps) => (
       <Component {...pageProps} />
       <Footer />
     </div>
+
+    {process.env.NODE_ENV !== 'development' &&
+    process.env.NEXT_PUBLIC_GA_TAG ? (
+      <>
+        <Script
+          strategy='afterInteractive'
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TAG}`}
+        />
+
+        <Script
+          id='gtag-init'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TAG}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </>
+    ) : null}
   </>
 );
 
