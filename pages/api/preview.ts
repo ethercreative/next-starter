b9972e9a9bb41ed4bfe8432f429a00e80b905b8c
James Edmonston <james@ethercreative.co.uk>
2022-07-05 12:08:46 +0100

    Add try/catch to preview route

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -3,34 +3,40 @@ import gql from 'fake-tag';
 import { client } from 'client';
 
 const preview = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (!req.query.entryUid) {
-    return res
-      .status(401)
-      .json({ message: 'Not allowed to access this route' });
-  }
+  try {
+    if (!req.query.entryUid) {
+      return res
+        .status(401)
+        .json({ message: 'Not allowed to access this route' });
+    }
 
-  const { entry } = await client().request(
-    gql`
-      query Preview($uid: String) {
-        entry(uid: [$uid], status: null) {
-          url
+    const { entry } = await client().request(
+      gql`
+        query Preview($uid: String!) {
+          entry(uid: [$uid], status: null) {
+            url
+          }
         }
-      }
-    `,
-    { uid: req.query.entryUid },
-  );
+      `,
+      { uid: req.query.entryUid },
+    );
 
-  if (!entry?.url) {
-    return res.status(404).json({
-      message: `URL of the entry "${req.query.entryUid}" could not be fetched`,
+    if (!entry?.url) {
+      return res.status(404).json({
+        message: `URL of the entry "${req.query.entryUid}" could not be fetched`,
+      });
+    }
+
+    res.setPreviewData({
+      previewToken: req.query.token ?? null,
+    });
+
+    res.redirect(entry.url);
+  } catch (e) {
+    return res.status(500).json({
+      message: e.message,
     });
   }
-
-  res.setPreviewData({
-    previewToken: req.query.token ?? null,
-  });
-
-  res.redirect(entry.url);
 };
 
 export default preview;
